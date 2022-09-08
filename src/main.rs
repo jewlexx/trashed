9fc8ee3028b675e930e5940f73d459da80e26421
Juliette Cordor <grassojames5@gmail.com>
2022-09-08 18:04:15 +1000

    implemented functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[clap(author, about)]
+struct Cli {
+    files: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    let canon_paths = args
+        .files
+        .iter()
+        .map(|f| {
+            dunce::canonicalize(f)
+                .unwrap_or_else(|_| panic!("Failed to canonicalize path (invalid path: {})", f))
+        })
+        .collect::<Vec<_>>();
+
+    trash::delete_all(canon_paths).expect("failed to move files to trash");
 }
